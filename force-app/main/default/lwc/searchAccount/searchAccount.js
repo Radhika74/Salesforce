1096b2d5ab4a032219dac2d9576c85e2a259c5da
Radhika Thakur <radhikathakur1103@gmail.com>
2025-04-23 21:41:25 +0530

    lwc done

--- force-app/main/default/lwc/searchAccount/searchAccount.js
+++ force-app/main/default/lwc/searchAccount/searchAccount.js
@@ -1,14 +1,12 @@
-import fetchDataAccount from '@salesforce/apex/AccountProvider.fetchDataAccount';
 import { LightningElement } from 'lwc';
-
-
+import fetchDataAccount from '@salesforce/apex/AccountProvider.fetchDataAccount';
 export default class SearchAccount extends LightningElement {
+
     objAcc={"sObjectType":"Account"}
     createDataHandler(){
         console.log('you are in function');
         this.objAcc.Name=this.template.querySelector('lightning-input[data-formfield="Acc"]').value;
         console.log(this.objAcc.Name);
-
         fetchDataAccount({objNewAcc:this.objAcc})
         .then(result=>{
             console.log(result);
